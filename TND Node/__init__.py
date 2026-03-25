86245c539b330141e5dd5fa2abc4f5388b80f2bd
JayHsueh007 <xuejianreedjourney@gmail.com>
2026-03-25 13:44:31 +0800

    Delete TND Node directory

--- TND Node/__init__.py
+++
@@ -1,6 +0,0 @@
-from .TND_resolution_classifier import NODE_CLASS_MAPPINGS as res_mappings, NODE_DISPLAY_NAME_MAPPINGS as res_display
-from .TND_aspect_ratio import NODE_CLASS_MAPPINGS as ar_mappings, NODE_DISPLAY_NAME_MAPPINGS as ar_display
-
-NODE_CLASS_MAPPINGS = {**res_mappings, **ar_mappings}
-NODE_DISPLAY_NAME_MAPPINGS = {**res_display, **ar_display}
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
